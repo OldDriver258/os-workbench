7877561b3350f1b191bb0352fba131aef7d64ab2
tracer-nju <tracer@nju.edu.cn>
2024-07-03 21:29:22 +0800

    Linux LAPTOP-EBMVO0I4 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:29:22 up  7:08,  1 user,  load average: 0.26, 0.17, 0.13

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+    int opt, opt_index = 0;
+    struct option pstree_option[] = {
+        {"show-pids", no_argument, NULL, 'p'},
+        {"numeric-sort", no_argument, NULL, 'n'},
+        {"version", no_argument, NULL, 'V'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "pnV", pstree_option, &opt_index)) != -1) {
+        switch (opt)
+        {
+        case 'p':
+            printf("find p flag\n");
+            break;
+        
+        case 'n':
+            printf("find n flag\n");
+            break;
+
+        case 'V':
+            fprintf(stderr, "pstree 0.1\n");
+            return 0;
+
+        default:
+            fprintf(stderr, "Usage: pstree [-p --show-pids] [-n --numeric-sort]\n");
+            fprintf(stderr, "       pstree -V --version\n");
+            return 1;
+        }
+    }
+
+    return 0;
 }
